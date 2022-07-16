ed77e7755182fa8c201be0ab2d8c300a07aed8d0
SHewitt95 <hewitt.sherman@gmail.com>
2022-07-16 11:12:31 -0400

    add button

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,18 @@
   import ProgressBar from "./lib/ProgressBar.svelte";
 
   let count: number = 0;
+  const RATE_OF_CHANGE: number = 1/12;
+  const MAX_VALUE: number = 100;
+  const MIN_VALUE: number = 0;
 
   function gameLoop() {
-    count = Math.min(count += (1/12), 100);
+    count = Math.max(Math.min(count += RATE_OF_CHANGE, MAX_VALUE), MIN_VALUE);
     window.requestAnimationFrame(gameLoop)
   }
 
+  function decrement() {
+    count -= 5;
+  }
 
   window.requestAnimationFrame(gameLoop);
 </script>
@@ -15,6 +21,7 @@
 <main>
   <ProgressBar value={count} />
   <p>The count is {count}</p>
+  <button on:click="{decrement}">Play Flute</button>
 </main>
 
 <style>
