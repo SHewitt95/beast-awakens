8dd3c4a0ce287466701420d8350e7f16b9a61e39
SHewitt95 <hewitt.sherman@gmail.com>
2022-07-16 17:19:04 -0400

    add ResetButton

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import ProgressBar from "./lib/ProgressBar.svelte";
   import FluteButton from "./lib/FluteButton.svelte";
   import BeastStatus from "./lib/BeastStatus.svelte";
+  import ResetButton from "./lib/ResetButton.svelte";
   import { MAX_COUNT_VALUE, MIN_COUNT_VALUE, INITIAL_RATE_OF_CHANGE } from "./lib/ReadableStore";
   import { count, money, gameOver } from "./lib/WritableStore";
 
@@ -20,6 +21,9 @@
   <ProgressBar value={$count} />
   <BeastStatus />
   <FluteButton />
+  {#if $gameOver}
+    <ResetButton />
+  {/if}
   <p>Money: {$money}</p>
 </main>
 
---
+++ src/lib/ResetButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { count, money, gameOver } from "./WritableStore";
+
+    function reset() {
+        $count = 0;
+        $money = 0;
+        $gameOver = false;
+    }
+</script>
+
+<button on:click="{reset}">Reset</button>
