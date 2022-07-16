b5aea4dace62c708b5316789b54d2696883bc8f6
SHewitt95 <hewitt.sherman@gmail.com>
2022-07-16 11:34:49 -0400

    add beastStatus, money

--- src/App.svelte
+++ src/App.svelte
@@ -6,13 +6,43 @@
   const MAX_VALUE: number = 100;
   const MIN_VALUE: number = 0;
 
+  let clickCount: number = 0;
+  let money: number = 0;
+
+  let gameOver: boolean = false;
+
+  let buttonText: string = "Play Flute";
+
+  let beastStatus: string = getBeastStatus(count);
+
   function gameLoop() {
     count = Math.max(Math.min(count += RATE_OF_CHANGE, MAX_VALUE), MIN_VALUE);
+    beastStatus = getBeastStatus(count);
+    if (count === MAX_VALUE) {
+      gameOver = true;
+      buttonText = "Game Over!";
+    }
     window.requestAnimationFrame(gameLoop)
   }
 
   function decrement() {
     count -= 5;
+    clickCount += 1;
+    money = clickCount % 10 === 0 ? money += 1 : money;
+  }
+
+  function getBeastStatus(count: number): string {
+    let status: string;
+
+    if (count === MAX_VALUE) {
+      status = "The Beast Awakens!"
+    } else if (count >= 75) {
+      status = "The Beast Stirs...";
+    } else {
+      status = "The Beast Rests.";
+    }
+
+    return status;
   }
 
   window.requestAnimationFrame(gameLoop);
@@ -20,8 +50,9 @@
 
 <main>
   <ProgressBar value={count} />
-  <p>The count is {count}</p>
-  <button on:click="{decrement}">Play Flute</button>
+  <p>{beastStatus}</p>
+  <button disabled={gameOver} on:click="{decrement}">{buttonText}</button>
+  <p>Money: {money}</p>
 </main>
 
 <style>
