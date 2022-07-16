dcb114c2be4be054f65a8d6ed469dec88d9a5272
SHewitt95 <hewitt.sherman@gmail.com>
2022-07-16 10:57:23 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import ProgressBar from "./lib/ProgressBar.svelte";
+
+  let count: number = 0;
+
+  function gameLoop() {
+    count = Math.min(count += (1/12), 100);
+    window.requestAnimationFrame(gameLoop)
+  }
+
+
+  window.requestAnimationFrame(gameLoop);
+</script>
+
+<main>
+  <ProgressBar value={count} />
+  <p>The count is {count}</p>
+</main>
+
+<style>
+  
+</style>
---
+++ src/lib/ProgressBar.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  export let max: number = 100;
+  export let value: number = 50;
+</script>
+
+<progress max={max} value={value}></progress>
